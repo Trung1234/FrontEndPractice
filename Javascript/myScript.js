368ec616a38bd75916686c29062ed29e951a593b
Trung1234 <trungsieunhan24@gmail.com>
2019-10-15 23:07:39 +0700

    init JS file

---
+++ Javascript/myScript.js
@@ -0,0 +1,9 @@
+function change() {
+    document.getElementById("demo").innerHTML = " changed.";
+    alert(document.getElementById("demo").innerHTML);
+    alert("--------------------");
+}
+function changeImg() {
+    alert(document.getElementById("demo").innerHTML);
+    alert("--------------------");
+}
