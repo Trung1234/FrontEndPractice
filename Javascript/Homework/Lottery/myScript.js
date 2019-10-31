f21cf90bf5c7df02b71f12623cc27941fbf0f5be
Trung1234 <trungsieunhan24@gmail.com>
2019-10-31 20:34:08 +0700

    done lottery

--- Javascript/Homework/Lottery/myScript.js
+++ Javascript/Homework/Lottery/myScript.js
@@ -1,10 +1,13 @@
   
 alert("Xin mời bạn nhập số!!!");
-function myFu(){
+function dial(){
     var out = Math.floor(Math.random() * 10);
     var input = document.getElementById("input").value;
     document.getElementById("output").innerHTML = out.toString();
-    if(input == out){
+    if(input == null || input.length==0){
+        document.getElementById("result").innerHTML = "Try again!!";
+    }
+    else if(input == out){
         document.getElementById("result").innerHTML = "Congratulation! you have a price!!";
         
     }else{
