3ae2b1b8e5e26e8b94e6eec1ff60d634241d896c
Trung1234 <trungsieunhan24@gmail.com>
2019-11-03 17:45:27 +0700

    done addment 5 js

---
+++ Javascript/Homework/Assigment5/myScript.js
@@ -0,0 +1,39 @@
+  
+
+function generate(){
+    var firstname = document.getElementById("firtname").value;
+    var lastname = document.getElementById("lastname").value;
+    document.getElementById("fullname").value
+     = firstname +" "+lastname ;
+
+}
+function finalGenerate(){
+    var fullname = document.getElementById("fullname").value
+    var color = document.getElementById("color").value;
+    var font = document.getElementById("font").value;
+    var type = document.getElementById("type").value;
+    document.getElementById("final").innerHTML = fullname;
+    document.getElementById("final").style.color = color;
+    document.getElementById("final").style.fontSize = font;
+    document.getElementById("final").style.fontStyle = type;
+}
+
+function validate(){
+    var fullname = document.getElementById("fullname").value
+    var color = document.getElementById("color").value;
+    var font = document.getElementById("font").value;
+    var type = document.getElementById("type").value;
+    if(fullname.length==0){
+        alert("hãy nhập vào tên");
+    }
+    else if(font.length==0){
+        alert("hãy nhập vào font chữ");
+    }
+    else if(color.length==0){
+        alert("hãy nhập vào màu sắc");
+    }
+    else if(color.charAt(0)!='#' || color.length!=7){
+        alert("màu sắc định dạng sai");
+    }
+
+}
