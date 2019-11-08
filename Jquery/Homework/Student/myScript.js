3d42db5ebd3fad6ed2b2c0b3702e05843e45626b
Trung1234 <trungsieunhan24@gmail.com>
2019-11-08 10:53:06 +0700

    done jquery assigment

--- Jquery/Homework/Student/myScript.js
+++ Jquery/Homework/Student/myScript.js
@@ -3,41 +3,44 @@
 
 
 function addRow(){
-    var i = $('#students tr').length-1;
-
+    resetError();
+    var tableRows = $('#students tr').length;
     var name = $("#name").val();
-    var category = $("#class").val();
+    var classOfStudent = $("#class").val();
     var math = Number($("#math").val());
     var physic = Number($("#physic").val());
     var chemical = Number($("#chemical").val());
-    i++;
     if(name.length==0){
-        $("#error").html("Họ tên bắt buộc phải nhập");
-        // alert("Họ tên bắt buộc phải nhập");
+        $("#nameError").css("color", "red").text("Họ tên bắt buộc phải nhập");
     }else{
-        if(category.length==0){
-            alert("Lớp học bắt buộc phải nhập");
+        if(classOfStudent.length==0){       
+            $("#classError").css("color", "red").text("Lớp học bắt buộc phải nhập");
         }else{
-            if(math.length==0 || math < 0 || math > 10){
-                alert("Điểm toán bắt buộc phải nhập");
+            if(math.length==0 || math <= 0 || math > 10){
+                $("#mathError").css("color", "red").text("Điểm toán bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
             }else{
-                if(physic.length==0 || physic < 0 || physic > 10){
-                    alert("Điểm lý bắt buộc phải nhập");
+                if(physic.length==0 || physic <= 0 || physic > 10){
+                    $("#physicError").css("color", "red").text("Điểm lý bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
                 }else{
-                    if(chemical.length==0 || chemical < 0 || chemical > 10){
-                        alert("Điểm hóa bắt buộc phải nhập");
+                    if(chemical.length==0 || chemical <= 0 || chemical > 10){
+                        $("#chemicalError").css("color", "red").text("Điểm hóa bắt buộc phải nhập và phải trong khoảng từ 0 đến 10");
                     }else{
                         $(document).ready(function () {
-                            $('#students').append('<tr><td>'+i+'</td><td>'+name+'</td><td>'
-                            +category+'</td><td>'+math+'</td><td>'+physic+'</td><td>'+chemical+'</td></tr>');
+                            $('#students').append('<tr><td>'+tableRows+
+                            '</td><td style="text-align:left;">'+name+'</td><td>'
+                            +classOfStudent+'</td><td>'+math+'</td><td>'
+                            +physic+'</td><td>'+chemical+'</td></tr>');
                         });
                     }
                 }
             }
         }
-    }
-
-    
-    
-
+    }    
+}
+function resetError(){
+    $("#nameError").text("");
+    $("#classError").text("");
+    $("#mathError").text("");
+    $("#physicError").text("");
+    $("#chemicalError").text("");
 }
